4c6b671f754b42e690922937ae3eac5b6ae1909f
Kha Nguyen <kha.t.nguyen.1@gmail.com>
2025-04-13 14:48:30 -0500

    chore: update lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,27 @@
-import { defineConfig } from "eslint/config";
-import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import tsParser from '@typescript-eslint/parser';
-import tseslintPlugin from '@typescript-eslint/eslint-plugin';
+import eslint from "@eslint/js"
+import tseslintPlugin from "@typescript-eslint/eslint-plugin"
+import tsParser from "@typescript-eslint/parser"
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended"
+import { defineConfig } from "eslint/config"
+import tseslint from "typescript-eslint"
 
 export default defineConfig([
   eslintPluginPrettierRecommended,
   eslint.configs.recommended,
   tseslint.configs.recommended,
-	{
-    files: ['**/*.ts', '**/*.tsx'], // Apply to TypeScript files
+  {
+    files: ["**/*.ts", "**/*.tsx"], // Apply to TypeScript files
     languageOptions: {
-        parser: tsParser, // Use the TypeScript parser
-        parserOptions: {
-            project: './tsconfig.json', // Specify your project's tsconfig.json
-        },
+      parser: tsParser, // Use the TypeScript parser
+      parserOptions: {
+        project: "./tsconfig.json", // Specify your project's tsconfig.json
+      },
     },
     plugins: {
-        '@typescript-eslint': tseslintPlugin, // Add the TypeScript ESLint plugin
+      "@typescript-eslint": tseslintPlugin, // Add the TypeScript ESLint plugin
     },
     rules: {
-        // Add TypeScript-specific rules if needed
+      // Add TypeScript-specific rules if needed
     },
-	},
-]);
-
+  },
+])
---
+++ prettier.config.js
@@ -0,0 +1,37 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  semi: false,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  importOrder: [
+    "^(react/(.*)$)|^(react$)",
+    "^(next/(.*)$)|^(next$)",
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "^types$",
+    "^@/types/(.*)$",
+    "^@/config/(.*)$",
+    "^@/lib/(.*)$",
+    "^@/hooks/(.*)$",
+    "^@/components/(.*)$",
+    "^@/registry/(.*)$",
+    "^@/styles/(.*)$",
+    "^[./]",
+  ],
+  importOrderSeparation: false,
+  importOrderSortSpecifiers: true,
+  importOrderBuiltinModulesToTop: true,
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+  importOrderMergeDuplicateImports: true,
+  importOrderCombineTypeAndValueImports: true,
+  plugins: [
+    "@ianvs/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss",
+  ],
+}
+
+export default config
