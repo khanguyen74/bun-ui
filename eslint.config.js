86ee39c7e491f634ad613f73fe5f635693c6d928
Kha Nguyen <kha.t.nguyen.1@gmail.com>
2025-04-13 14:01:46 -0500

    chore: setup prettier and lint

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from "eslint/config";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import tsParser from '@typescript-eslint/parser';
+import tseslintPlugin from '@typescript-eslint/eslint-plugin';
+
+export default defineConfig([
+  eslintPluginPrettierRecommended,
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+	{
+    files: ['**/*.ts', '**/*.tsx'], // Apply to TypeScript files
+    languageOptions: {
+        parser: tsParser, // Use the TypeScript parser
+        parserOptions: {
+            project: './tsconfig.json', // Specify your project's tsconfig.json
+        },
+    },
+    plugins: {
+        '@typescript-eslint': tseslintPlugin, // Add the TypeScript ESLint plugin
+    },
+    rules: {
+        // Add TypeScript-specific rules if needed
+    },
+	},
+]);
+
