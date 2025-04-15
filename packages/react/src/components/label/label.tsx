a5d64df2f2902214592163ffc81a4685f054bb92
Kha Nguyen <kha.t.nguyen.1@gmail.com>
2025-04-14 19:57:41 -0500

    feat: add select component

--- packages/react/src/components/label/label.tsx
+++ packages/react/src/components/label/label.tsx
@@ -1,20 +1,18 @@
 import React from "react"
 import * as LabelPrimitives from "@radix-ui/react-label"
-import { cva, type VariantProps } from "class-variance-authority"
 
 import { cx } from "../../lib/utils"
 
-const labelVariants = cva(
-  "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-)
 const Label = React.forwardRef<
   React.ComponentRef<typeof LabelPrimitives.Root>,
-  React.ComponentPropsWithoutRef<typeof LabelPrimitives.Root> &
-    VariantProps<typeof labelVariants>
+  React.ComponentPropsWithoutRef<typeof LabelPrimitives.Root>
 >(({ className, ...props }, ref) => (
   <LabelPrimitives.Root
     ref={ref}
-    className={cx(labelVariants(), className)}
+    className={cx(
+      "text-sm leading-none font-medium peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
+      className
+    )}
     {...props}
   />
 ))
---
+++ packages/react/src/components/select/select.tsx
@@ -0,0 +1,112 @@
+import React from "react"
+import * as SelectPrimitives from "@radix-ui/react-select"
+import { Check, ChevronDown } from "lucide-react"
+
+import { cx } from "../../lib/utils"
+import { Label } from "../label"
+
+interface SelectProps
+  extends React.ComponentPropsWithoutRef<typeof SelectPrimitives.Root> {
+  placeholder?: React.ReactNode
+  className?: string
+  /**
+   * The label for the select input.
+   **/
+  label?: React.ReactNode
+  /**
+   * The position of the menu. Default is "popper".
+   **/
+  menuPosition?: "item-aligned" | "popper"
+}
+
+const Select = React.forwardRef<
+  React.ComponentRef<typeof SelectPrimitives.Root>,
+  SelectProps
+>(
+  (
+    {
+      className,
+      placeholder,
+      children,
+      value,
+      menuPosition = "popper",
+      label,
+      ...props
+    },
+    ref
+  ) => (
+    <div className="flex flex-col gap-2">
+      {label && <Label>{label}</Label>}
+      <SelectPrimitives.Root {...props}>
+        <SelectPrimitives.Trigger
+          ref={ref}
+          className={cx(
+            "focus:ring-ring bg-background data-[placeholder]:text-muted-foreground border-input ring-offset-background flex h-10 w-full items-center justify-between rounded-md border px-3 py-2 text-sm focus:ring-2 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed disabled:opacity-50",
+            className
+          )}
+        >
+          <SelectPrimitives.Value placeholder={placeholder}>
+            {value}
+          </SelectPrimitives.Value>
+          <SelectPrimitives.Icon>
+            <ChevronDown className="h-4 w-4" />
+          </SelectPrimitives.Icon>
+        </SelectPrimitives.Trigger>
+
+        <SelectPrimitives.Portal>
+          <SelectPrimitives.Content
+            position={menuPosition}
+            className={cx(
+              "bg-popover text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 relative z-50 max-h-[--radix-select-content-available-height] min-w-[8rem] origin-[--radix-select-content-transform-origin] overflow-x-hidden overflow-y-auto rounded-md border shadow-md",
+              menuPosition === "popper" &&
+                "data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1"
+            )}
+          >
+            <SelectPrimitives.Viewport
+              className={cx(
+                "p-1",
+                menuPosition === "popper" &&
+                  "h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)]"
+              )}
+            >
+              {children}
+            </SelectPrimitives.Viewport>
+          </SelectPrimitives.Content>
+        </SelectPrimitives.Portal>
+      </SelectPrimitives.Root>
+    </div>
+  )
+)
+
+const SelectItem = React.forwardRef<
+  React.ComponentRef<typeof SelectPrimitives.Item>,
+  React.ComponentPropsWithoutRef<typeof SelectPrimitives.Item>
+>(({ className, children, value, ...props }, ref) => (
+  <SelectPrimitives.Item
+    ref={ref}
+    className={cx(
+      "data-[highlighted]:bg-accent/40 data-[state=checked]:bg-accent relative flex w-full cursor-default items-center rounded-sm py-1 pl-5 text-sm outline-none select-none",
+      className
+    )}
+    value={value}
+    {...props}
+  >
+    <SelectPrimitives.ItemIndicator className="absolute left-1 flex items-center justify-center">
+      <Check className="h-3 w-3" />
+    </SelectPrimitives.ItemIndicator>
+    <SelectPrimitives.ItemText>{children}</SelectPrimitives.ItemText>
+  </SelectPrimitives.Item>
+))
+const SelectLabel = React.forwardRef<
+  React.ComponentRef<typeof SelectPrimitives.Label>,
+  React.ComponentPropsWithoutRef<typeof SelectPrimitives.Label>
+>(({ className, ...props }, ref) => (
+  <SelectPrimitives.Label
+    ref={ref}
+    className={cx("py-1 pl-5 text-sm font-semibold", className)}
+    {...props}
+  />
+))
+const SelectGroup = SelectPrimitives.Group
+
+export { Select, SelectItem, type SelectProps, SelectGroup, SelectLabel }
