f2ae4dce6bbcad5bdbabf9e09f07c48d5bb8b638
Kha Nguyen <kha.t.nguyen.1@gmail.com>
2025-04-16 23:06:38 -0500

    fix: generate id for form components for accessibility
    - generated id associates label and the form component

--- packages/react/src/components/checkbox/checkbox.tsx
+++ packages/react/src/components/checkbox/checkbox.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useId } from "react"
 import * as CheckboxPrimitive from "@radix-ui/react-checkbox"
 import { Check } from "lucide-react"
 
@@ -13,24 +13,37 @@ interface CheckboxProps
 const Checkbox = React.forwardRef<
   React.ComponentRef<typeof CheckboxPrimitive.Root>,
   CheckboxProps
->(({ className, label, id, ...props }, ref) => (
-  <div className="flex items-center space-x-2">
-    <CheckboxPrimitive.Root
-      ref={ref}
-      id={id}
-      className={cx(
-        "peer border-primary ring-offset-background focus-visible:ring-ring data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground h-4 w-4 shrink-0 rounded-xs border focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50",
-        className
+>(({ className, label, id, disabled, ...props }, ref) => {
+  const _id = id || useId()
+  return (
+    <div className="flex items-center space-x-2">
+      <CheckboxPrimitive.Root
+        ref={ref}
+        id={_id}
+        className={cx(
+          "peer border-primary ring-offset-background focus-visible:ring-ring data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground disabled:cursor-not-allow h-4 w-4 shrink-0 cursor-pointer rounded-xs border focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:opacity-50",
+          className
+        )}
+        disabled={disabled}
+        {...props}
+      >
+        <CheckboxPrimitive.CheckboxIndicator
+          className={cx("flex items-center justify-center text-current")}
+        >
+          <Check className="h-4 w-4" />
+        </CheckboxPrimitive.CheckboxIndicator>
+      </CheckboxPrimitive.Root>
+      {label && (
+        <Label
+          className={cx("cursor-pointer", disabled && "cursor-not-allowed")}
+          htmlFor={_id}
+        >
+          {label}
+        </Label>
       )}
-      {...props}
-    >
-      <CheckboxPrimitive.CheckboxIndicator className="flex items-center justify-center text-current">
-        <Check className="h-4 w-4" />
-      </CheckboxPrimitive.CheckboxIndicator>
-    </CheckboxPrimitive.Root>
-    {label && <Label htmlFor={id}>{label}</Label>}
-  </div>
-))
+    </div>
+  )
+})
 
 Checkbox.displayName = "Checkbox"
 
--- packages/react/src/components/input/input.tsx
+++ packages/react/src/components/input/input.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useId } from "react"
 import { type ClassValue } from "clsx"
 
 import { cx } from "../../lib/utils"
@@ -10,12 +10,16 @@ export interface InputProps extends React.ComponentProps<"input"> {
 }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, label, labelClassName, ...props }, ref) => {
+  ({ className, label, labelClassName, id, ...props }, ref) => {
+    const _id = id ?? useId()
     if (label) {
       return (
         <div className="flex flex-col gap-2">
-          <Label className={cx(labelClassName)}>{label}</Label>
+          <Label htmlFor={_id} className={cx(labelClassName)}>
+            {label}
+          </Label>
           <input
+            id={_id}
             ref={ref}
             className={cx(
               "border-input bg-background ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring flex h-10 w-full rounded-md border px-3 py-2 text-base file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
@@ -28,6 +32,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
     }
     return (
       <input
+        id={_id}
         ref={ref}
         className={cx(
           "border-input bg-background ring-offset-background file:text-foreground placeholder:text-muted-foreground focus-visible:ring-ring flex h-10 w-full rounded-md border px-3 py-2 text-base file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
