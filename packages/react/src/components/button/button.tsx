f006ad52d1d53b463c7c3945089ce9087b98ced9
Kha Nguyen <kha.t.nguyen.1@gmail.com>
2025-04-17 10:36:16 -0500

    feat: add hover effect to button and tabs

--- packages/react/src/components/button/button.tsx
+++ packages/react/src/components/button/button.tsx
@@ -5,19 +5,20 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cx } from "../../lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 cursor-pointer",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 cursor-pointer",
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default:
+          "bg-primary text-primary-foreground hover:not-disabled:bg-primary/90",
         destructive:
-          "bg-destructive text-destructive-foreground hover:bg-destructive/90",
+          "bg-destructive text-destructive-foreground hover:not-disabled:bg-destructive/90",
         outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+          "border border-input bg-background hover:not-disabled:bg-accent hover:not-disabled:text-accent-foreground",
         secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",
+          "bg-secondary text-secondary-foreground hover:not-disabled:bg-secondary/80",
+        ghost: "hover:not-disabled:bg-accent hover:text-accent-foreground",
+        link: "text-primary underline-offset-4 hover:not-disabled:underline",
       },
       size: {
         default: "h-10 px-4 py-2",
