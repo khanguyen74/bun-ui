3d202ab9ba25b17cb2aef64a45081ce55c01ee2f
Kha Nguyen <kha.t.nguyen.1@gmail.com>
2025-04-17 23:59:20 -0500

    docs: fix some layout issues on mobile view

--- apps/www/src/components/toc.tsx
+++ apps/www/src/components/toc.tsx
@@ -29,27 +29,26 @@ export const Toc = ({ items }: Props) => {
   if (!items.length) return <div />
 
   return (
-    <aside
-      id="toc"
-      className="sticky top-20 -mt-6 h-[calc(100vh-3.5rem)] w-[16rem] pt-6 text-sm"
-    >
-      <p className="font-semibold text-gray-900 dark:text-white">
-        On this page
-      </p>
-      <div className="mt-3 flex flex-col space-y-2">
-        {items.map((item, index) => (
-          <Link
-            key={index}
-            href={item.url}
-            data-toc
-            id={item.url}
-            aria-current={item.url === activeItem ? "page" : undefined}
-            className={`text-gray-500 transition-colors hover:text-gray-900 dark:text-gray-400 dark:hover:text-white ${item.url === activeItem ? "font-medium text-gray-900 dark:text-white" : ""} `}
-            style={{ marginLeft: `calc(1rem * ${item.depth})` }}
-          >
-            {item.title}
-          </Link>
-        ))}
+    <aside id="toc" className="hidden xl:block">
+      <div className="sticky top-20 -mt-6 h-[calc(100vh-3.5rem)] w-[16rem] pt-6 text-sm">
+        <p className="font-semibold text-gray-900 dark:text-white">
+          On this page
+        </p>
+        <div className="mt-3 flex flex-col space-y-2">
+          {items.map((item, index) => (
+            <Link
+              key={index}
+              href={item.url}
+              data-toc
+              id={item.url}
+              aria-current={item.url === activeItem ? "page" : undefined}
+              className={`text-gray-500 transition-colors hover:text-gray-900 dark:text-gray-400 dark:hover:text-white ${item.url === activeItem ? "font-medium text-gray-900 dark:text-white" : ""} `}
+              style={{ marginLeft: `calc(1rem * ${item.depth})` }}
+            >
+              {item.title}
+            </Link>
+          ))}
+        </div>
       </div>
     </aside>
   )
--- apps/www/src/examples/button-variants.tsx
+++ apps/www/src/examples/button-variants.tsx
@@ -2,7 +2,7 @@ import { Button } from "@bun-ui/react"
 
 export const ButtonVariants = () => {
   return (
-    <div className="flex gap-4">
+    <div className="flex flex-col gap-4 md:flex-row">
       <Button>Default</Button>
       <Button variant="secondary">Secondary</Button>
       <Button variant="outline">Outline</Button>
