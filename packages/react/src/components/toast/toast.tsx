fd75718ed9f465307db498e1e730c5bb84c65833
Kha Nguyen <kha.t.nguyen.1@gmail.com>
2025-04-24 20:00:49 -0500

    update export

--- packages/react/src/components/toast/toast.tsx
+++ packages/react/src/components/toast/toast.tsx
@@ -62,7 +62,7 @@ const ToastClose = React.forwardRef<
   <ToastPrimitives.Close
     ref={ref}
     className={cx(
-      "text-foreground/50 hover:text-foreground absolute top-2 right-2 rounded-md p-1 opacity-0 transition-opacity group-hover:opacity-100 group-[.destructive]:text-red-300 group-[.destructive]:hover:text-red-50 focus:opacity-100 focus:ring-2 focus:outline-none group-[.destructive]:focus:ring-red-400 group-[.destructive]:focus:ring-offset-red-600",
+      "text-foreground/50 hover:text-foreground absolute top-2 right-2 cursor-pointer rounded-md p-1 opacity-0 transition-opacity group-hover:opacity-100 group-[.destructive]:text-red-300 group-[.destructive]:hover:text-red-50 focus:opacity-100 focus:ring-2 focus:outline-none group-[.destructive]:focus:ring-red-400 group-[.destructive]:focus:ring-offset-red-600",
       className
     )}
     toast-close=""
--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -2,3 +2,4 @@ import "./styles/global.css"
 
 export * from "./components"
 export * from "./lib"
+export * from "./hooks"
