c7b09f738418904ba31acdf95f5b75b914c0c123
Kha Nguyen <kha.t.nguyen.1@gmail.com>
2025-04-25 19:04:16 -0500

    lint fix

--- packages/react/tsup.config.ts
+++ packages/react/tsup.config.ts
@@ -1,7 +1,7 @@
 import fs from "fs"
 import path from "path"
-import { defineConfig, Options } from "tsup"
 import glob from "fast-glob"
+import { defineConfig, Options } from "tsup"
 
 const componentEntries = glob.sync("src/components/*/index.ts").map((file) => {
   const name = path.basename(path.dirname(file)) // e.g. "button"
@@ -23,23 +23,28 @@ export default defineConfig(() => [
     splitting: false,
     css: true,
   },
-  ...componentEntries.map(({ entry, outDir }) => ({
-    entry: [entry],
-    outDir,
-    format: ["esm"],
-    dts: true,
-    sourcemap: true,
-    clean: false,
-    css: true,
-    onSuccess: async () => {
-      const toastFile = path.resolve("dist/toast/index.mjs")
-      if (fs.existsSync(toastFile)) {
-        const content = fs.readFileSync(toastFile, "utf8")
-        if (!content.startsWith('"use client"')) {
-          fs.writeFileSync(toastFile, `"use client";\n\n${content}`)
-          console.log(`✅ Added "use client" to ${toastFile}`)
-        }
-      }
-    },
-  }) as Options),
+  ...componentEntries.map(
+    ({ entry, outDir }) =>
+      ({
+        entry: [entry],
+        outDir,
+        format: ["esm"],
+        dts: true,
+        sourcemap: true,
+        clean: false,
+        css: true,
+        onSuccess: async () => {
+          if (outDir === "dist/toast") {
+            const toastFile = path.resolve("dist/toast/index.mjs")
+            if (fs.existsSync(toastFile)) {
+              const content = fs.readFileSync(toastFile, "utf8")
+              if (!content.startsWith('"use client"')) {
+                fs.writeFileSync(toastFile, `"use client";\n\n${content}`)
+                console.log(`✅ Added "use client" to ${toastFile}`)
+              }
+            }
+          }
+        },
+      }) as Options
+  ),
 ])
