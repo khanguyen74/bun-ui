24f68f29e76fe2908128dea8eef0d18877eab466
Kha Nguyen <kha.t.nguyen.1@gmail.com>
2025-04-29 22:27:46 -0500

    feat: update useTheme hook

--- packages/react/src/hooks/use-theme/index.ts
+++ packages/react/src/hooks/use-theme/index.ts
@@ -6,12 +6,37 @@ import { useCallback } from "react"
 import { useTheme as useNextTheme } from "next-themes"
 
 const useTheme = () => {
-  const { resolvedTheme: nextTheme, setTheme: setNextTheme } = useNextTheme()
+  const { resolvedTheme, setTheme } = useNextTheme()
 
-  const toggleTheme = useCallback(() => {
-    setNextTheme(nextTheme === "dark" ? "light" : "dark")
-  }, [nextTheme, setNextTheme])
-  return { theme: nextTheme, toggleTheme }
+  const toggleDarkMode = useCallback(() => {
+    if (!resolvedTheme) return
+
+    const isDark = resolvedTheme.includes("dark")
+    const isExactDark = resolvedTheme === "dark"
+    const isExactLight = resolvedTheme === "light"
+
+    if (isExactDark) {
+      setTheme("light")
+    } else if (isExactLight) {
+      setTheme("dark")
+    } else {
+      const newTheme = isDark
+        ? resolvedTheme.replace(/-?dark/, "")
+        : `${resolvedTheme}-dark`
+      setTheme(newTheme)
+    }
+  }, [resolvedTheme])
+
+  return {
+    theme: resolvedTheme,
+    /**
+     * Switches between light and dark mode.
+     * If the theme is "light", it changes to "dark", and if it's "dark", it changes to "light".
+     * For custom themes, it adds or removes "-dark" from the theme name.
+     */
+    toggleDarkMode,
+    setTheme,
+  }
 }
 
 export { useTheme }
