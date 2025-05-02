405da97b5e8c50afb2ac15f1969972636c021c04
Kha Nguyen <kha.t.nguyen.1@gmail.com>
2025-05-01 22:39:27 -0500

    feat: update Toggle style

--- packages/react/src/components/toggle/toggle.tsx
+++ packages/react/src/components/toggle/toggle.tsx
@@ -5,13 +5,13 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cx } from "../../lib"
 
 const toggleVariants = cva(
-  "data-[state=on]:bg-foreground/20 hover:not-disabled:not-data-[state=on]:bg-foreground/10 focus-visible:ring-ring inline-flex items-center justify-center rounded-sm hover:not-disabled:cursor-pointer focus-visible:ring-1 focus-visible:outline-none disabled:pointer-events-none text-foreground ",
+  "data-[state=on]:bg-foreground/20 hover:not-disabled:not-data-[state=on]:bg-foreground/10 focus-visible:ring-ring inline-flex items-center justify-center rounded-sm hover:not-disabled:cursor-pointer focus-visible:ring-1 focus-visible:outline-none disabled:pointer-events-none text-foreground",
   {
     variants: {
       size: {
-        sm: "h-6 min-w-6 px-1.5",
-        md: "h-8 min-w-8 px-2",
-        lg: "h-10 min-w-10 px-2.5",
+        sm: "h-6 min-w-6 px-1.5 [&_svg]:w-4 [&_svg]:h-4",
+        md: "h-8 min-w-8 px-2 [&_svg]:w-5 [&_svg]:h-5",
+        lg: "h-10 min-w-10 px-2.5 [&_svg]:w-6 [&_svg]:h-6",
       },
     },
     defaultVariants: {
@@ -27,12 +27,14 @@ interface ToggleProps
 const Toggle = React.forwardRef<
   React.ComponentRef<typeof TogglePrimitive.Root>,
   ToggleProps
->(({ className, size, ...props }, ref) => (
-  <TogglePrimitive.Root
-    ref={ref}
-    className={cx(toggleVariants({ className, size }))}
-    {...props}
-  />
-))
+>(({ className, size, ...props }, ref) => {
+  return (
+    <TogglePrimitive.Root
+      ref={ref}
+      className={cx(toggleVariants({ className, size }))}
+      {...props}
+    />
+  )
+})
 
 export { Toggle, toggleVariants }
