4d13089d0bb8e1322f99dc180ba4243239febfcd
Kha Nguyen <kha.t.nguyen.1@gmail.com>
2025-05-01 23:55:11 -0500

    chore: change some examples

--- apps/www/src/examples/button-spinner.tsx
+++ apps/www/src/examples/button-spinner.tsx
@@ -3,7 +3,7 @@ import { Button, Spinner } from "@bun-ui/react"
 export const ButtonSpinner = () => {
   return (
     <Button>
-      <Spinner size="sm" color="primary" />
+      <Spinner size="sm" color="current" />
       Loading...
     </Button>
   )
