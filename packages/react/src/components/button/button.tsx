d2ac4cf8cf54100810ad5bda3fbf95fc1ddb26be
Kha Nguyen <kha.t.nguyen.1@gmail.com>
2025-05-01 23:54:58 -0500

    fix: update Button style

--- packages/react/src/components/button/button.tsx
+++ packages/react/src/components/button/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cx } from "../../lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 cursor-pointer disabled:[&_*]:pointer-events-none",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-70 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 cursor-pointer disabled:[&_*]:pointer-events-none",
   {
     variants: {
       variant: {
@@ -13,10 +13,10 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground hover:not-disabled:bg-destructive/90",
         outline:
-          "border border-input bg-background hover:not-disabled:bg-primary hover:not-disabled:text-primary-foreground",
+          "border border-primary/70 bg-background hover:not-disabled:border-primary hover:not-disabled:text-primary text-primary/70 ",
         secondary:
           "bg-secondary text-secondary-foreground hover:not-disabled:bg-secondary/80",
-        text: "hover:not-disabled:bg-secondary hover:text-secondary-foreground",
+        text: "hover:not-disabled:bg-primary/10 hover:text-foreground",
         link: "text-primary underline-offset-4 hover:not-disabled:underline",
       },
       size: {
