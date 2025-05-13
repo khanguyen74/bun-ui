faec7124711ebec8a7dadbd3cb3de8dadd2af942
Kha Nguyen <kha.t.nguyen.1@gmail.com>
2025-05-13 16:32:21 -0500

    chore(www): put icons in components

--- apps/www/src/components/main-nav.tsx
+++ apps/www/src/components/main-nav.tsx
@@ -1,13 +1,13 @@
 import Link from "next/link"
-import Logo from "@/icons/logo.svg"
 
+import { BunUIIcon } from "./icons"
 import { VersionBadge } from "./version-badge"
 
 export function MainNav() {
   return (
     <div className="hidden items-center gap-2 lg:flex">
       <Link href="/" className="flex items-center gap-2 px-2">
-        <Logo width={42} height={42} />
+        <BunUIIcon width={42} height={42} />
         <span className="hidden font-mono text-sm font-bold lg:inline-block">
           Bun UI
         </span>
