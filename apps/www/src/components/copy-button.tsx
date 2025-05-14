f600df036071e1a929a1d32b64848522ae0ded64
Kha Nguyen <kha.t.nguyen.1@gmail.com>
2025-05-14 10:57:26 -0500

    chore(www): update copy button

--- apps/www/src/components/copy-button.tsx
+++ apps/www/src/components/copy-button.tsx
@@ -4,8 +4,6 @@ import React from "react"
 import { Button, ButtonProps } from "@bun-ui/react"
 import { CheckIcon, ClipboardIcon } from "lucide-react"
 
-import { cx } from "@/lib/classnames"
-
 interface CopyButtonProps extends ButtonProps {
   value: string
   src?: string
@@ -33,10 +31,8 @@ export function CopyButton({
     <Button
       variant={variant}
       size="icon"
-      className={cx(
-        "relative z-10 text-zinc-50 hover:bg-zinc-700 hover:text-zinc-50 [&_svg]:h-4 [&_svg]:w-4",
-        className
-      )}
+      color="neutral"
+      className={className}
       onClick={() => {
         copyToClipboardWithMeta(value)
         setHasCopied(true)
