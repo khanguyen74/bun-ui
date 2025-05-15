7aac15da5c8bc5c41ec42c1d5c980412575a323d
Kha Nguyen <kha.t.nguyen.1@gmail.com>
2025-05-14 22:55:55 -0500

    fix: update button size

--- packages/react/src/components/button/button.tsx
+++ packages/react/src/components/button/button.tsx
@@ -21,9 +21,9 @@ const buttonVariants = cva(
         neutral: "",
       },
       size: {
-        md: "h-10 px-4 py-2",
-        sm: "h-9 px-3",
-        lg: "h-11 px-8",
+        sm: "px-3 py-1",
+        md: "px-4 py-2",
+        lg: "px-6 py-2 text-base",
         icon: "h-10 w-10",
       },
     },
