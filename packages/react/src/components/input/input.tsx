e12ca7015ba6d39c915e0fedfadbfadf5a436db9
Kha Nguyen <kha.t.nguyen.1@gmail.com>
2025-05-19 20:04:45 -0500

    fix(Input): fix label layout

--- packages/react/src/components/input/input.tsx
+++ packages/react/src/components/input/input.tsx
@@ -1,45 +1,35 @@
 import React, { useId } from "react"
-import { type ClassValue } from "clsx"
 
 import { cx } from "../../lib/utils"
 import { Label } from "../label"
 
 export interface InputProps extends React.ComponentProps<"input"> {
   label?: React.ReactNode
-  labelClassName?: ClassValue
+  labelClassName?: string
 }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, label, labelClassName, id, ...props }, ref) => {
     const _id = id ?? useId()
-    if (label) {
-      return (
-        <div className="flex flex-col gap-2">
-          <Label htmlFor={_id} className={cx(labelClassName)}>
+    const Wrapper = label ? "div" : React.Fragment
+    const wrapperClassName = label ? "flex flex-col gap-2" : ""
+    return (
+      <Wrapper {...(label ? { className: wrapperClassName } : {})}>
+        {label && (
+          <Label htmlFor={_id} className={labelClassName}>
             {label}
           </Label>
-          <input
-            id={_id}
-            ref={ref}
-            className={cx(
-              "border-input bg-background ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring flex h-10 w-full rounded-md border px-3 py-2 text-base file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-              className
-            )}
-            {...props}
-          />
-        </div>
-      )
-    }
-    return (
-      <input
-        id={_id}
-        ref={ref}
-        className={cx(
-          "border-input bg-background ring-offset-background file:text-foreground placeholder:text-muted-foreground focus-visible:ring-ring flex h-10 w-full rounded-md border px-3 py-2 text-base file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-          className
         )}
-        {...props}
-      />
+        <input
+          id={_id}
+          ref={ref}
+          className={cx(
+            "border-input bg-background ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring flex h-10 w-full rounded-md border px-3 py-2 text-base file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+            className
+          )}
+          {...props}
+        />
+      </Wrapper>
     )
   }
 )
